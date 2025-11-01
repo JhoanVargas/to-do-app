f3c2e7ff985756cd10a427cf381de9a206fd3fff
JhoanVargas <jhoan.vargas@davinci.tech>
2025-11-01 18:21:59 -0500

    ajustes ionic build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD00cm4eM_EuzOuufEXHToYPUVglBKlKdM',
+    authDomain: 'to-do-9ea63.firebaseapp.com',
+    projectId: 'to-do-9ea63',
+    storageBucket: 'to-do-9ea63.firebasestorage.app',
+    messagingSenderId: '171232633633',
+    appId: '1:171232633633:web:51e1c930b6d2a110f11460',
+  },
 };
